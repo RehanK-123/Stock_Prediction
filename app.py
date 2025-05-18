b1c58603ae2acf386d2307e464f5f26501642928
Rehan Shahid Khan <153989998+RehanK-123@users.noreply.github.com>
2025-05-18 05:32:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ model.fit(X_train, y_train, epochs=20, batch_size=16, validation_data=(X_test, y
 
 @app.route("/", methods= ["GET"])
 def index():
-    return render_template("index.html", output= 267)
+    date_input = request.form.get("date")
+    return render_template("index.html", output= date_input)
 
 @app.route("/favicon.ico")
 def favicon():
